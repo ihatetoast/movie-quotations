da2e26c879eaabc98542b7d434a74c0f7e903b6c
Katy <ihatetoast@gmail.com>
2017-03-25 21:54:30 -0500

    changing to ES6

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A generic assignment we all do, but it plays with AJAX calls, Twitter buttons, and position relative and absolute.
 
 [Movie Quotation Generator on Surge](http://movie-quotations-ihatetoast.surge.sh)
+
+Updated: Have changed to ES6 syntax.
--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 $("#twitterBtn").on("click", function(){
 	window.open('http://twitter.com/intent/tweet?text=' + $('#movieQuote').text() + " " + $('#quoteMovie').text());
 	});
-	$("#newQteBtn").on("click", function(){
+	$("#newQteBtn").on("click", ()=>{
+		let quotations = (data)=>{
+			console.log("success!", data);
+			$("#movieScreen").html('<span id="movieQuote" class="quoteFont">"'+data.quote+'"</span></br><span id="quoteMovie" class="movieFont">&#8211 from '+data.author+'</span>');
+		}
 		$.ajax({
 			url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies',
 			type: 'GET',
 			success: quotations,
 			dataType: 'json',
-			beforeSend: function(xhr) {
+			beforeSend: (xhr) =>{
 			xhr.setRequestHeader("X-Mashape-Key", "y1jJJUQiSSmshlFwDrFXvHmCiZX5p1MGrFQjsnGWVaLtxF600l"); // Enter here your Mashape key
 			},
-			error: function(err){
+			error: (err) =>{
 				console.log("error: ",err);
 			},
-			complete: function(){
+			complete: () =>{
 				console.log("I got a response.");
 			}
 		});
-		function quotations(data){
-			console.log("success!", data);
-			$("#movieScreen").html('<span id="movieQuote" class="quoteFont">"'+data.quote+'"</span></br><span id="quoteMovie" class="movieFont">&#8211 from '+data.author+'</span>');
-		}
+		
 	});
